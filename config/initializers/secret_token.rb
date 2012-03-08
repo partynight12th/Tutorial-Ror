6c4a6a086e35a6e3b7ea8f7221221e8e9cddd6f1
partynight12th <utageyanagi+github@gmail.com>
2012-03-09 01:06:50 +0900

    FirstCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nksoft::Application.config.secret_token = '2ab6306ba033d6bc7a16b4a021ef2c4f664d7637cc67ec718e36be70f09466b0378ba37a909fd9e94ebaabdf09496a00102ef21fb1db97e4a53e38e62ed280ba'
